0358260bf8677b9c345843b13e311c76c8ebf039
kuntal <km7999319@gmail.com>
2024-07-14 22:35:16 +0530

    chore: Update clerkMiddleware to use latest version

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/','/api/webhooks/clerk','/api/webhooks/stripe','/sign-in(.*)', '/sign-up(.*)']);
-
-export default clerkMiddleware((auth, request) => {
-  if(!isPublicRoute(request)) {
-    auth().protect();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
